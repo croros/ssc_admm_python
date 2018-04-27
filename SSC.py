54c94049dc9411fcb50ce59720c43591f973a069
Constantine <croros26@gmail.com>
2018-04-26 21:14:49 -0400

    working on actual ADMM section now

--- SSC.py
+++ SSC.py
@@ -1,26 +1,8 @@
 import numpy as np
 import sys
 
-def SSC(X,r,isAffine,alpha,hasOutlier,rho,s):
+def SSC(X,r=0,isAffine=False,alpha=20,hasOutlier=False,rho=1,s):
 
-	numArgs = len(sys.argv)
-
-	if numArgs < 6:
-		rho = 1
-
-	if numArgs < 5:
-		hasOutlier = False
-
-	if numArgs < 4:
-		alpha = 20
-
-	if numArgs < 3:
-		isAffine = True
-
-	if numArgs < 2:
-		r = 0
-		
-		
 	n = np.max(s) #Number of groups moving (ground truth)
 	#Xp = DataProjection(X,r) #Use PCA to project onto subspace r (not used for motion segmentation
 	Xp = X
@@ -42,4 +24,53 @@ def SSC(X,r,isAffine,alpha,hasOutlier,rho,s):
 	missrate = Misclasification(grps,s)
 	
 	return missrate, Cout, grps
+
+def admmLasso(Y,isAffine=False,alpha=800,thresh=0.0002,maxIter=200):
+	
+	#Check if alpha is a single value or multiple
+	if isinstance(alpha,int) or isinstance(alpha,float):
+		alpha1 = alpha #Used for lambda_z (penalty/regularization parameter of noise)
+		alpha2 = alpha #Used for rho (penalty parameter/step size used for updating the lagrange multipliers deltaBig and deltaLil )
+	else:
+		alpha1 = alpha[0]
+		alpha2 = alpha[1]
+		
+	if isinstance(thresh,int) or isinstance(thresh,float):
+		thresh1 = thresh #Threshold on maximum error between Auxiliary variable A and original variable C (goal is A=C)
+		
+		#used for initial L2-norm error of Y-Y*A (i.e. E^k-E^{k-1}). 
+		#E is the outlier coefficient matrix, so its not really needed here and its recorded for sake of interest.
+		thresh2 = thresh 
+	
+	N = np.size(Y,1)
+	
+	#setting penalty parameters for the ADMM
+	lambda_z = alpha1/(getMu(Y))
+	rho = alpha2
+	
+	#Further computations need us to distinguish linear and affine subspaces
+	if isAffine:
+		firstA = np.linalg.inv(lambda_z*(dot(Y.T,Y))+rho*(np.eye(N))+rho*np.ones((N,N))#First part of A (doesn't change so precompute)
+		C_k = np.zeros((N,N))
+		deltaBig = np.zeros((N,N))
+		deltaLil = np.zeros((1,N))
+		
+		errIdx = 0
+		errAC = np.zeros((1,maxIter))
+		errAC[errIdx] = 10*thresh1
+		errE = np.zeros((1,maxIter))
+		errE[errIdx] = 10*thresh2
+		errAt1 = np.zeros((1,maxIter))
+		errAt1[errIdx] = 10*thresh1
+		
+		#Now that parameters are set up, begin ADMM iterations
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	
